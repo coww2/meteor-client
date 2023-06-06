33f73512540ea8410546bf3762e551be7d1772de
coww2 <135682606+coww2@users.noreply.github.com>
2023-06-06 02:43:56 -0500

    Update StashFinder.java

--- src/main/java/meteordevelopment/meteorclient/systems/modules/world/StashFinder.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/world/StashFinder.java
@@ -26,6 +26,13 @@ import meteordevelopment.meteorclient.utils.Utils;
 import meteordevelopment.meteorclient.utils.render.MeteorToast;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.block.entity.*;
+import net.minecraft.block.entity.BarrelBlockEntity;
+import net.minecraft.block.entity.ChestBlockEntity;
+import net.minecraft.block.entity.DispenserBlockEntity;
+import net.minecraft.block.entity.EnderChestBlockEntity;
+import net.minecraft.block.entity.HopperBlockEntity;
+import net.minecraft.block.entity.ShulkerBoxBlockEntity;
+import net.minecraft.block.entity.AbstractFurnaceBlockEntity;
 import net.minecraft.item.Items;
 import net.minecraft.util.math.ChunkPos;
 
@@ -48,7 +55,7 @@ public class StashFinder extends Module {
     );
 
     private final Setting<Integer> minimumStorageCount = sgGeneral.add(new IntSetting.Builder()
-        .name("minimum-storage-cont")
+        .name("minimum-storage-count")
         .description("The minimum amount of storage blocks in a chunk to record the chunk.")
         .defaultValue(4)
         .min(1)
@@ -83,236 +90,120 @@ public class StashFinder extends Module {
     public List<Chunk> chunks = new ArrayList<>();
 
     public StashFinder() {
-        super(Categories.World, "stash-finder", "Searches loaded chunks for storage blocks. Saves to <your minecraft folder>/meteor-client");
+        super(Categories.World, "stash-finder", "Finds chunks with storage blocks.");
     }
 
     @Override
     public void onActivate() {
-        load();
-    }
-
-    @EventHandler
-    private void onChunkData(ChunkDataEvent event) {
-        // Check the distance.
-        double chunkXAbs = Math.abs(event.chunk.getPos().x * 16);
-        double chunkZAbs = Math.abs(event.chunk.getPos().z * 16);
-        if (Math.sqrt(chunkXAbs * chunkXAbs + chunkZAbs * chunkZAbs) < minimumDistance.get()) return;
-
-        Chunk chunk = new Chunk(event.chunk.getPos());
-
-        for (BlockEntity blockEntity : event.chunk.getBlockEntities().values()) {
-            if (!storageBlocks.get().contains(blockEntity.getType())) continue;
-
-            if (blockEntity instanceof ChestBlockEntity) chunk.chests++;
-            else if (blockEntity instanceof BarrelBlockEntity) chunk.barrels++;
-            else if (blockEntity instanceof ShulkerBoxBlockEntity) chunk.shulkers++;
-            else if (blockEntity instanceof EnderChestBlockEntity) chunk.enderChests++;
-            else if (blockEntity instanceof AbstractFurnaceBlockEntity) chunk.furnaces++;
-            else if (blockEntity instanceof DispenserBlockEntity) chunk.dispensersDroppers++;
-            else if (blockEntity instanceof HopperBlockEntity) chunk.hoppers++;
-        }
-
-        if (chunk.getTotal() >= minimumStorageCount.get()) {
-            Chunk prevChunk = null;
-            int i = chunks.indexOf(chunk);
-
-            if (i < 0) chunks.add(chunk);
-            else prevChunk = chunks.set(i, chunk);
-
-            saveJson();
-            saveCsv();
-
-            if (sendNotifications.get() && (!chunk.equals(prevChunk) || !chunk.countsEqual(prevChunk))) {
-                switch (notificationMode.get()) {
-                    case Chat -> info("Found stash at (highlight)%s(default), (highlight)%s(default).", chunk.x, chunk.z);
-                    case Toast -> mc.getToastManager().add(new MeteorToast(Items.CHEST, title, "Found Stash!"));
-                    case Both -> {
-                        info("Found stash at (highlight)%s(default), (highlight)%s(default).", chunk.x, chunk.z);
-                        mc.getToastManager().add(new MeteorToast(Items.CHEST, title, "Found Stash!"));
-                    }
-                }
-            }
-        }
-
-        ChunkDataEvent.returnChunkDataEvent(event);
+        chunks.clear();
     }
 
     @Override
     public WWidget getWidget(GuiTheme theme) {
-        // Sort
-        chunks.sort(Comparator.comparingInt(value -> -value.getTotal()));
-
         WVerticalList list = theme.verticalList();
 
-        // Clear
-        WButton clear = list.add(theme.button("Clear")).widget();
+        WTable table = list.add(theme.table()).expandX().widget();
 
-        WTable table = new WTable();
-        if (chunks.size() > 0) list.add(table);
+        table.add(theme.label("Chunk"));
+        table.add(theme.label("Storages"));
 
-        clear.action = () -> {
+        chunks.sort(Comparator.comparingInt(Chunk::getStorageCount).reversed());
+
+        for (Chunk chunk : chunks) {
+            WButton button = table.add(theme.button(chunk.getPos().toShortString())).expandCellX().widget();
+            button.action = () -> {
+                BaritoneAPI.getProvider().getPrimaryBaritone().getCustomGoalProcess().setGoalAndPath(new GoalXZ(chunk.getPos().x, chunk.getPos().z));
+                Utils.mc.setScreen(new WindowScreen(new ChunkScreen(chunk, theme)));
+            };
+
+            table.add(theme.label(String.valueOf(chunk.getStorageCount())));
+        }
+
+        list.add(theme.horizontalSeparator());
+
+        WMinus minus = list.add(theme.minus()).widget();
+        minus.action = () -> {
             chunks.clear();
-            table.clear();
+            MeteorToast.showToast(theme, "Cleared stashes.", MeteorToast.Type.Info);
         };
-
-        // Chunks
-        fillTable(theme, table);
 
         return list;
     }
 
-    private void fillTable(GuiTheme theme, WTable table) {
-        for (Chunk chunk : chunks) {
-            table.add(theme.label("Pos: " + chunk.x + ", " + chunk.z));
-            table.add(theme.label("Total: " + chunk.getTotal()));
+    @EventHandler
+    private void onChunkData(ChunkDataEvent event) {
+        if (!event.shouldLoad()) return;
 
-            WButton open = table.add(theme.button("Open")).widget();
-            open.action = () -> mc.setScreen(new ChunkScreen(theme, chunk));
+        ChunkPos pos = event.chunk.getPos();
+        double distanceToSpawn = Math.sqrt(pos.x * pos.x + pos.z * pos.z);
 
-            WButton gotoBtn = table.add(theme.button("Goto")).widget();
-            gotoBtn.action = () -> BaritoneAPI.getProvider().getPrimaryBaritone().getCustomGoalProcess().setGoalAndPath(new GoalXZ(chunk.x, chunk.z));
+        if (distanceToSpawn < minimumDistance.get()) return;
 
-            WMinus delete = table.add(theme.minus()).widget();
-            delete.action = () -> {
-                if (chunks.remove(chunk)) {
-                    table.clear();
-                    fillTable(theme, table);
+        int storageCount = countStorageBlocks(event.chunk);
 
-                    saveJson();
-                    saveCsv();
+        if (storageCount >= minimumStorageCount.get()) {
+            Chunk chunk = new Chunk(pos, storageCount);
+
+            if (!chunks.contains(chunk)) {
+                chunks.add(chunk);
+                chunks.sort(Comparator.comparingInt(Chunk::getStorageCount).reversed());
+
+                if (sendNotifications.get()) {
+                    String message = "Found chunk " + chunk.getPos().toShortString() + " with " + chunk.getStorageCount() + " storage blocks.";
+
+                    if (notificationMode.get() == Mode.Chat || notificationMode.get() == Mode.Both) {
+                        info(message);
+                    }
+
+                    if (notificationMode.get() == Mode.Notification || notificationMode.get() == Mode.Both) {
+                        MeteorClient.toast().add(new MeteorToast.Notification("Stash Finder", message));
+                    }
                 }
-            };
-
-            table.row();
-        }
-    }
-
-    private void load() {
-        boolean loaded = false;
-
-        // Try to load json
-        File file = getJsonFile();
-        if (file.exists()) {
-            try {
-                FileReader reader = new FileReader(file);
-                chunks = GSON.fromJson(reader, new TypeToken<List<Chunk>>() {}.getType());
-                reader.close();
-
-                for (Chunk chunk : chunks) chunk.calculatePos();
-
-                loaded = true;
-            } catch (Exception ignored) {
-                if (chunks == null) chunks = new ArrayList<>();
-            }
-        }
-
-        // Try to load csv
-        file = getCsvFile();
-        if (!loaded && file.exists()) {
-            try {
-                BufferedReader reader = new BufferedReader(new FileReader(file));
-                reader.readLine();
-
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    String[] values = line.split(" ");
-                    Chunk chunk = new Chunk(new ChunkPos(Integer.parseInt(values[0]), Integer.parseInt(values[1])));
-
-                    chunk.chests = Integer.parseInt(values[2]);
-                    chunk.shulkers = Integer.parseInt(values[3]);
-                    chunk.enderChests = Integer.parseInt(values[4]);
-                    chunk.furnaces = Integer.parseInt(values[5]);
-                    chunk.dispensersDroppers = Integer.parseInt(values[6]);
-                    chunk.hoppers = Integer.parseInt(values[7]);
-
-                    chunks.add(chunk);
-                }
-
-                reader.close();
-            } catch (Exception ignored) {
-                if (chunks == null) chunks = new ArrayList<>();
             }
         }
     }
 
-    private void saveCsv() {
-        try {
-            File file = getCsvFile();
-            file.getParentFile().mkdirs();
-            Writer writer = new FileWriter(file);
+    private int countStorageBlocks(net.minecraft.world.chunk.WorldChunk chunk) {
+        int count = 0;
 
-            writer.write("X,Z,Chests,Barrels,Shulkers,EnderChests,Furnaces,DispensersDroppers,Hoppers\n");
-            for (Chunk chunk : chunks) chunk.write(writer);
-
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        for (BlockEntity blockEntity : chunk.getBlockEntities().values()) {
+            if (storageBlocks.get().contains(blockEntity.getType())) {
+                count++;
+            }
         }
-    }
 
-    private void saveJson() {
-        try {
-            File file = getJsonFile();
-            file.getParentFile().mkdirs();
-            Writer writer = new FileWriter(file);
-            GSON.toJson(chunks, writer);
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private File getJsonFile() {
-        return new File(new File(new File(MeteorClient.FOLDER, "stashes"), Utils.getFileWorldName()), "stashes.json");
-    }
-
-    private File getCsvFile() {
-        return new File(new File(new File(MeteorClient.FOLDER, "stashes"), Utils.getFileWorldName()), "stashes.csv");
+        return count;
     }
 
     @Override
-    public String getInfoString() {
-        return String.valueOf(chunks.size());
+    public void read(File file) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            chunks = GSON.fromJson(reader, new TypeToken<List<Chunk>>() {}.getType());
+        }
     }
 
-    public enum Mode {
-        Chat,
-        Toast,
-        Both
+    @Override
+    public void write(File file) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            writer.write(GSON.toJson(chunks));
+        }
     }
 
     public static class Chunk {
-        private static final StringBuilder sb = new StringBuilder();
+        private final ChunkPos pos;
+        private final int storageCount;
 
-        public ChunkPos chunkPos;
-        public transient int x, z;
-        public int chests, barrels, shulkers, enderChests, furnaces, dispensersDroppers, hoppers;
-
-        public Chunk(ChunkPos chunkPos) {
-            this.chunkPos = chunkPos;
-
-            calculatePos();
+        public Chunk(ChunkPos pos, int storageCount) {
+            this.pos = pos;
+            this.storageCount = storageCount;
         }
 
-        public void calculatePos() {
-            x = chunkPos.x * 16 + 8;
-            z = chunkPos.z * 16 + 8;
+        public ChunkPos getPos() {
+            return pos;
         }
 
-        public int getTotal() {
-            return chests + barrels + shulkers + enderChests + furnaces + dispensersDroppers + hoppers;
-        }
-
-        public void write(Writer writer) throws IOException {
-            sb.setLength(0);
-            sb.append(x).append(',').append(z).append(',');
-            sb.append(chests).append(',').append(barrels).append(',').append(shulkers).append(',').append(enderChests).append(',').append(furnaces).append(',').append(dispensersDroppers).append(',').append(hoppers).append('\n');
-            writer.write(sb.toString());
-        }
-
-        public boolean countsEqual(Chunk c) {
-            if (c == null) return false;
-            return chests != c.chests || barrels != c.barrels || shulkers != c.shulkers || enderChests != c.enderChests || furnaces != c.furnaces || dispensersDroppers != c.dispensersDroppers || hoppers != c.hoppers;
+        public int getStorageCount() {
+            return storageCount;
         }
 
         @Override
@@ -320,63 +211,19 @@ public class StashFinder extends Module {
             if (this == o) return true;
             if (o == null || getClass() != o.getClass()) return false;
             Chunk chunk = (Chunk) o;
-            return Objects.equals(chunkPos, chunk.chunkPos);
+            return Objects.equals(pos, chunk.pos);
         }
 
         @Override
         public int hashCode() {
-            return Objects.hash(chunkPos);
+            return Objects.hash(pos);
         }
     }
 
-    private static class ChunkScreen extends WindowScreen {
-        private final Chunk chunk;
-
-        public ChunkScreen(GuiTheme theme, Chunk chunk) {
-            super(theme, "Chunk at " + chunk.x + ", " + chunk.z);
-
-            this.chunk = chunk;
-        }
-
-        @Override
-        public void initWidgets() {
-            WTable t = add(theme.table()).expandX().widget();
-
-            // Total
-            t.add(theme.label("Total:"));
-            t.add(theme.label(chunk.getTotal() + ""));
-            t.row();
-
-            t.add(theme.horizontalSeparator()).expandX();
-            t.row();
-
-            // Separate
-            t.add(theme.label("Chests:"));
-            t.add(theme.label(chunk.chests + ""));
-            t.row();
-
-            t.add(theme.label("Barrels:"));
-            t.add(theme.label(chunk.barrels + ""));
-            t.row();
-
-            t.add(theme.label("Shulkers:"));
-            t.add(theme.label(chunk.shulkers + ""));
-            t.row();
-
-            t.add(theme.label("Ender Chests:"));
-            t.add(theme.label(chunk.enderChests + ""));
-            t.row();
-
-            t.add(theme.label("Furnaces:"));
-            t.add(theme.label(chunk.furnaces + ""));
-            t.row();
-
-            t.add(theme.label("Dispensers and droppers:"));
-            t.add(theme.label(chunk.dispensersDroppers + ""));
-            t.row();
-
-            t.add(theme.label("Hoppers:"));
-            t.add(theme.label(chunk.hoppers + ""));
-        }
+    public enum Mode {
+        Chat,
+        Notification,
+        Both
     }
 }
+
